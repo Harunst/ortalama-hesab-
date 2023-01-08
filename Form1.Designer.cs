a2d18a588f3cf035afd759889e6a2460ab902209
harun <harun@HARUN-TURUNC>
2023-01-08 23:29:02 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace ortalama_hesabı
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtYazili1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtYazili2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDevamsizlik = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(193, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Yazılı 1";
+            // 
+            // txtYazili1
+            // 
+            this.txtYazili1.Location = new System.Drawing.Point(253, 55);
+            this.txtYazili1.Name = "txtYazili1";
+            this.txtYazili1.Size = new System.Drawing.Size(100, 20);
+            this.txtYazili1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(173, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtYazili2
+            // 
+            this.txtYazili2.Location = new System.Drawing.Point(253, 81);
+            this.txtYazili2.Name = "txtYazili2";
+            this.txtYazili2.Size = new System.Drawing.Size(100, 20);
+            this.txtYazili2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(193, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Yazılı 2";
+            // 
+            // txtDevamsizlik
+            // 
+            this.txtDevamsizlik.Location = new System.Drawing.Point(253, 107);
+            this.txtDevamsizlik.Name = "txtDevamsizlik";
+            this.txtDevamsizlik.Size = new System.Drawing.Size(100, 20);
+            this.txtDevamsizlik.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(170, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Devamsızlık";
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Location = new System.Drawing.Point(185, 196);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(48, 13);
+            this.lblDurum.TabIndex = 7;
+            this.lblDurum.Text = "lblDurum";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 443);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.txtDevamsizlik);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtYazili2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtYazili1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtYazili1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtYazili2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDevamsizlik;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblDurum;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ortalama_hesabı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double devamsizlik = 0, yazili1 = 0, yazili2 = 0, ortalama = 0;
+            yazili1 = Convert.ToDouble(txtYazili1.Text);
+            yazili2 = Convert.ToDouble(txtYazili2.Text);
+            devamsizlik = Convert.ToDouble(txtDevamsizlik.Text);
+            ortalama = (yazili1 + yazili2) / 2;
+
+            if (devamsizlik >= 10)
+            {
+                lblDurum.Text = "Devamsızlıktan Kaldınız. Ortalamanız: " + ortalama;
+            }
+            else if (ortalama < 50)
+            {
+                lblDurum.Text = "Ortalama ile Kaldınız. Ortalamanız: " + ortalama;
+            }
+            else
+            {
+                lblDurum.Text = "Geçtiniz. Ortalamanız: " + ortalama;
+            }
+        }
+    }
+}
